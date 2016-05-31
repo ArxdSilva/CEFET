a9257c4903ef390f2fc43401368a110667f3a33d
Arthur Silva <arthur.silva@corp.globo.com>
2016-05-31 14:40:04 -0300

    revisao

---
+++ GloboCom/Exemplos/vehicle.py
@@ -0,0 +1,9 @@
+class Vehicle(object):
+    def __init__(self, comb_cons, comb_inicial, eficiencia):
+        self.comb_cons = comb_cons
+        self.comb_inicial = comb_inicial
+        self.eficiencia = eficiencia
+
+    def mover(self, km):
+        rodando = ((km/self.eficiencia) == )
+        if :
